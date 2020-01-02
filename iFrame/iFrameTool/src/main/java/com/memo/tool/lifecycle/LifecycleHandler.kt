5215ad33d9f5418269436680f16db2969b2250ac
周豪侠 <mdzz.zhou@gmail.com>
2020-01-02 14:38:56 +0800

    Handler优化

--- iFrame/iFrameTool/src/main/java/com/memo/tool/lifecycle/LifecycleHandler.kt
+++ iFrame/iFrameTool/src/main/java/com/memo/tool/lifecycle/LifecycleHandler.kt
@@ -1,32 +1,56 @@
 package com.memo.tool.lifecycle
 
 import android.os.Handler
+import android.os.Looper
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.OnLifecycleEvent
 
 /**
- * title:带有生命周期控制的Handler
- * describe: 如果只是使用延时功能 建议查看RxExt中的delay
+ * title: 带有生命周期控制的Handler
+ * describe:
  *
  * @author memo
- * @date 2019-11-20 14:18
+ * @date 2020-01-02 14:33
  * @email zhou_android@163.com
  *
  * Talk is cheap, Show me the code.
  */
-class LifecycleHandler(private val lifecycleOwner : LifecycleOwner, callback : Callback)
-	: Handler(callback), LifecycleObserver {
-	
-	
-	init {
-		lifecycleOwner.lifecycle.addObserver(this)
-	}
-	
-	@OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-	private fun onDestroy() {
-		removeCallbacksAndMessages(null)
-		lifecycleOwner.lifecycle.removeObserver(this)
-	}
+class LifecycleHandler : Handler, LifecycleObserver {
+
+    private var lifecycleOwner: LifecycleOwner
+
+    constructor(lifecycleOwner: LifecycleOwner) {
+        this.lifecycleOwner = lifecycleOwner
+        addObserver()
+    }
+
+    constructor(lifecycleOwner: LifecycleOwner, callback: Callback)
+            : super(callback) {
+        this.lifecycleOwner = lifecycleOwner
+        addObserver()
+    }
+
+    constructor(lifecycleOwner: LifecycleOwner, looper: Looper)
+            : super(looper) {
+        this.lifecycleOwner = lifecycleOwner
+        addObserver()
+    }
+
+    constructor(lifecycleOwner: LifecycleOwner, looper: Looper, callback: Callback)
+            : super(looper, callback) {
+        this.lifecycleOwner = lifecycleOwner
+        addObserver()
+    }
+
+    private fun addObserver() {
+        lifecycleOwner.lifecycle.addObserver(this)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    private fun onDestroy() {
+        removeCallbacksAndMessages(null)
+        lifecycleOwner.lifecycle.removeObserver(this)
+    }
 }
